d576f507703b852fc2f355640a23332457fecaad
Alexander Orvik <alexao1111@gmail.com>
2021-11-10 01:51:43 +0100

    feat(theme): add grayscales and default shadow

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,16 @@ export const theme = {
     primary: '#E87644',
     secondary: 'secondary',
 
+    gray1: '#333333',
+    gray2: '#BBBBBB',
+    gray3: 'CCCCCC',
+    gray4: 'DDDDDD',
+    gray5: 'EEEEEE',
+
+    background: '#f6f7fb',
+
+    white: '#FFFFFF',
+
     success: 'success',
     error: 'error',
     warning: 'warning',
@@ -11,6 +21,9 @@ export const theme = {
     mobile: '@media screen and (max-width: 768px)',
     smallScreen: '@media screen and (max-width: 400px)',
     largeScreen: '@media screen and (max-width: 1600px)',
+  },
+  shadow: {
+    default: '0 2px 6px 0 rgba(0, 0, 0, 0.2);',
   },
 
   resetButton: `
