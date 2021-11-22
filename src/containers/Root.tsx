a6055a2075f076d7712706783c059ba615bff72e
Magnus L. Holtet <dev@holtet.me>
2021-11-22 17:57:07 +0100

    Environment-handling

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -5,6 +5,8 @@ import client from 'apollo-setup'
 import Bootstrap from './BootStrap'
 
 function Root() {
+  console.log('ENV:', process.env.REACT_APP_NODE_ENV + ' 🚀')
+  console.log('URI:', process.env.REACT_APP_API_URL)
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
