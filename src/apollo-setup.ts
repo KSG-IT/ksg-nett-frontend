a43c50a1a64b5367c7aa396cf25f69c97590fac3
Alexander Orvik <alexao1111@gmail.com>
2021-11-22 15:12:10 +0100

    CI debug

--- src/apollo-setup.ts
+++ src/apollo-setup.ts
@@ -6,40 +6,37 @@ import { getLoginToken } from 'util/auth'
 import { API_URL } from 'util/env'
 
 const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = getLoginToken()
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      }
-    }
-  })
-  
+  // get the authentication token from local storage if it exists
+  const token = getLoginToken()
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+console.log(API_URL)
+
 // Log any GraphQL errors or network error that occurred
 const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      );
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  });
-  
-  const uploadLink = createUploadLink({
-    uri: API_URL + "/graphql/"
-  })
-  
-  
-  const client = new ApolloClient({
-    link: ApolloLink.from([
-      errorLink,
-      authLink.concat(uploadLink)
-    ]),
-    cache: new InMemoryCache()
-  })
-  
-  export default client
-  
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+
+const uploadLink = createUploadLink({
+  uri: API_URL + '/graphql/',
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([errorLink, authLink.concat(uploadLink)]),
+  cache: new InMemoryCache(),
+})
+
+export default client
