58d7b229b0c3284ccee6d8a3e43b72dba93a2f80
Alexander Orvik <alexao1111@gmail.com>
2021-11-26 18:23:12 +0100

    chore(login): use theme instead of hardcoded string in css

--- src/modules/login/Login.tsx
+++ src/modules/login/Login.tsx
@@ -58,7 +58,7 @@ const StyledInput = styled.input`
 const LoginTitle = styled.h1`
   font-size: 46px;
   margin-bottom: 15px;
-  color: #701fea;; //${props => props.theme.colors.purple};
+  color: ${props => props.theme.colors.purpleAction};
   font-weight: 600;
 `
 
