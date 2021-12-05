8ab4bb8ea8db0968faca0779687543859dd95b92
Alexander Orvik <alexao1111@gmail.com>
2021-12-05 19:34:07 +0100

    feat(economy): mutation, queries and typing

--- src/modules/economy/types.ts
+++ src/modules/economy/types.ts
@@ -61,12 +61,13 @@ export interface TransferNode {
 }
 
 export interface DepositNode {
+  id: string
   account: SociBankAccountNode
   description: string
   amount: number
   receipt: string // string with image url
-  signOffBy: UserNode
-  signedOffTime: Date
+  signedOffBy: UserNode | null
+  signedOffTime: Date | null
   approved: boolean
   comments: DepositCommentNode[]
 }
