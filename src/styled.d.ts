fa31ebe51833aa24c350e30d5b69b98c908c491e
Alexander Orvik <alexao1111@gmail.com>
2021-12-14 16:52:21 +0100

    refactor styled components theme declaration

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,8 +1,7 @@
 import 'styled-components'
 import theme from './theme'
 
-export type CustomThemeType = typeof theme
-
 declare module 'styled-components' {
-  export interface DefaultTheme extends CustomThemeType {}
+  type Theme = typeof theme
+  export interface DefaultTheme extends Theme {}
 }
