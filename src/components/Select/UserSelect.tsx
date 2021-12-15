3abd0840d0c16c4b0bf1a0cca9c1a95545a00546
Alexander Orvik <alexao1111@gmail.com>
2021-12-15 17:07:50 +0100

    feat(components): implement UserSelect component
    Add a component to select a single user which is searchable
    	* Executes query onLoad and stores all user within component
    	* Search is local filtering
    	* Accepts downstream value and callback so that the component
    	can be used together with forms

---
+++ src/components/Select/UserSelect.tsx
@@ -0,0 +1,204 @@
+import { UserNode, UserThumbnail } from 'modules/users'
+import { useState, useEffect, useCallback } from 'react'
+import styled from 'styled-components'
+import {
+  ALL_ACTIVE_USERS_SHALLOW_QUERY,
+  AllUsersShallowQueryReturns,
+  AllUsersShallowQueryVariables,
+} from 'modules/users'
+import { useQuery } from '@apollo/client'
+import OutsideClickHandler from 'react-outside-click-handler'
+import { ZIndexRange } from 'types/enums'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 400px;
+  position: relative;
+  background-color: ${props => props.theme.colors.white};
+  border-radius: 10px;
+  box-shadow: ${props => props.theme.shadow.default};
+  margin: 0;
+  z-index: ${ZIndexRange.Dropdowns};
+`
+const SelectedUser = styled.span`
+  font-size: 18px;
+`
+const SelectWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 45px;
+  padding: 5px;
+`
+
+interface OpenProps {
+  open: boolean
+}
+
+const DropdownContainer = styled.div<OpenProps>`
+  display: ${props => (props.open ? 'flex' : 'none')};
+  flex-direction: column;
+  width: 100%;
+  max-height: 400px;
+  background-color: ${props => props.theme.colors.white};
+  position: absolute;
+  border-radius: 0 0 10px 10px;
+  box-shadow: ${props => props.theme.shadow.default};
+  top: 45px;
+`
+
+const UserSearchContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  padding: 10px;
+`
+const UserSearch = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 5px 0;
+  background-color: ${props => props.theme.colors.white};
+`
+
+const UserSearchInput = styled.input`
+  outline: none;
+  width: 100%;
+  height: 100%;
+  border: none;
+  border-radius: 5px;
+`
+const ResultContainer = styled.div`
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+const ResultRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  height: 40px;
+  padding: 0 5px;
+  font-size: 16px;
+  justify-content: space-between;
+  &:hover {
+    background-color: ${props => props.theme.colors.lightGray};
+  }
+`
+
+const ResultRowName = styled.span``
+
+const Chevron = styled(FontAwesomeIcon)<OpenProps>`
+  transform: ${props => (props.open ? 'rotate(180deg)' : 'rotate(0deg)')};
+  transition: transform 250ms ease-in-out;
+`
+interface UserSelectProps {
+  reporter: String
+  setReporterCallback: (slectedId: string) => void
+}
+
+type ShallowUser = Pick<
+  UserNode,
+  'id' | 'profileImage' | 'fullName' | 'initials'
+>
+
+export const UserSelect: React.VFC<UserSelectProps> = ({
+  reporter,
+  setReporterCallback,
+}) => {
+  /*
+   * Improvements:
+   *   - Enter handling -> Autoselect top selection
+   */
+
+  const [selectDisplayName, setSelectDisplayName] = useState('')
+  const [search, setSearch] = useState('')
+  const [users, setUsers] = useState<ShallowUser[]>([])
+  const [filteredUsers, setFilteredUsers] = useState<ShallowUser[]>([])
+  const [open, setOpen] = useState(false)
+
+  useQuery<AllUsersShallowQueryReturns, AllUsersShallowQueryVariables>(
+    ALL_ACTIVE_USERS_SHALLOW_QUERY,
+    {
+      onCompleted({ allActiveUsers }) {
+        const flattenedUsers = allActiveUsers.edges.map(edge => edge.node)
+        setUsers(flattenedUsers)
+        setFilteredUsers(flattenedUsers)
+      },
+    }
+  )
+
+  useEffect(() => {
+    const result = users.filter(user => {
+      const parsedName = user.fullName.toLowerCase()
+      const parsedSearch = search.toLowerCase()
+      return parsedName.includes(parsedSearch)
+    })
+    setFilteredUsers(result)
+  }, [search, setFilteredUsers, users])
+
+  const handleSelectUser = useCallback(
+    user => {
+      setReporterCallback(user.id)
+      setSelectDisplayName(user.fullName)
+      setOpen(false)
+      setSearch('')
+    },
+    [setSearch, setOpen, setSelectDisplayName, setReporterCallback]
+  )
+
+  const handleToggleSelect = useCallback(() => {
+    setOpen(!open)
+  }, [open, setOpen])
+
+  const handleClickOutside = () => {
+    setOpen(false)
+  }
+  return (
+    <OutsideClickHandler onOutsideClick={handleClickOutside}>
+      <Wrapper>
+        <SelectWrapper onClick={handleToggleSelect}>
+          <SelectedUser>{selectDisplayName}</SelectedUser>
+          <Chevron icon="chevron-down" size="sm" open={open} />
+        </SelectWrapper>
+        <DropdownContainer open={open}>
+          <UserSearchContainer>
+            <UserSearch>
+              <UserSearchInput
+                value={search}
+                onChange={evt => setSearch(evt.target.value)}
+                placeholder="Søk etter bruker"
+              />
+              <FontAwesomeIcon icon="search" size="sm" />
+            </UserSearch>
+          </UserSearchContainer>
+          <ResultContainer>
+            {filteredUsers.map(user => (
+              <ResultRow
+                key={user.id}
+                onClick={() => {
+                  handleSelectUser(user)
+                }}
+              >
+                <ResultRowName>{user.fullName}</ResultRowName>
+                {/* Thumbnail needs a size between tiny and small */}
+                <UserThumbnail user={user} size="small" />
+              </ResultRow>
+            ))}
+          </ResultContainer>
+        </DropdownContainer>
+      </Wrapper>
+    </OutsideClickHandler>
+  )
+}
