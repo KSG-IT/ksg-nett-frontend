ee83b7eb2d4a0f2789840b124b9ad8f2740acb63
Alexander Orvik <alexao1111@gmail.com>
2022-01-16 16:21:34 +0100

    feat: implement new sidebar in MainLayout.tsx

--- src/containers/MainLayout.tsx
+++ src/containers/MainLayout.tsx
@@ -1,78 +1,30 @@
-import styled, { css } from 'styled-components'
-import { useState } from 'react'
-import { AuthRoutes } from 'containers//AuthRoutes'
-import { Sidebar } from 'modules/sidebar'
+import { Layout } from 'antd'
 import { Header } from 'modules/header'
-import { useRenderMobile } from 'util/isMobile'
-interface WrapperProps {
-  sidebarOpen: boolean
-}
+import { ReworkedSidebar } from 'modules/sidebar'
+import React, { useState } from 'react'
+import { AuthRoutes } from './AuthRoutes'
 
-const Wrapper = styled.div<WrapperProps>`
-  display: grid;
-  width: 100%;
-  grid-template-columns: 300px calc(100vw - 300px);
-  grid-template-rows: 70px calc(100vh - 70px);
-  grid-template-areas:
-    'sidebar header'
-    'sidebar main';
-
-  ${props => props.theme.media.mobile} {
-    display: flex;
-    flex-direction: column;
-    ${props =>
-      props.sidebarOpen &&
-      css`
-        overflow-y: auto;
-      `};
-  }
-`
-
-interface ContentWrapperProps {
-  visible: boolean
-}
-
-const ContentWrapper = styled.div<ContentWrapperProps>`
-  grid-area: main;
-  display: ${props => (props.visible ? 'none' : 'flex')};
-  background-color: ${props => props.theme.colors.background};
-`
-
-const HeaderWrapper = styled.div`
-  grid-area: header;
-  width: 100%;
-  height: 100%;
-`
-
-const SidebarWrapper = styled.div`
-  grid-area: sidebar;
-`
+const { Header: AntHeader, Content } = Layout
 
 const MainLayout: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
-  const isMobile = useRenderMobile()
 
-  const shouldNotRenderContent = isMobile && sidebarOpen
-
-  const toggleSidebarCallback = () => {
+  const toggleOpen = () => {
     setSidebarOpen(!sidebarOpen)
   }
 
   return (
-    <Wrapper sidebarOpen={true}>
-      <HeaderWrapper>
-        <Header toggleSidebar={toggleSidebarCallback}></Header>
-      </HeaderWrapper>
-      <SidebarWrapper>
-        <Sidebar
-          sidebarOpen={sidebarOpen}
-          toggleSidebarCallback={toggleSidebarCallback}
-        />
-      </SidebarWrapper>
-      <ContentWrapper visible={shouldNotRenderContent}>
-        <AuthRoutes />
-      </ContentWrapper>
-    </Wrapper>
+    <Layout hasSider>
+      <ReworkedSidebar open={sidebarOpen} />
+      <Layout>
+        <AntHeader>
+          <Header toggleSidebar={() => toggleOpen()} />
+        </AntHeader>
+        <Content>
+          <AuthRoutes />
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 
