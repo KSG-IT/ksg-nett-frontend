c7119402768b69b658b5f39ee92c44c688514af1
Alexander Orvik <alexao1111@gmail.com>
2022-01-20 16:57:47 +0100

    chore(containers): correct component typing
    Bootstrap -> FC to VFC

--- src/containers/BootStrap.tsx
+++ src/containers/BootStrap.tsx
@@ -1,13 +1,12 @@
+import { useQuery } from '@apollo/client'
 import MainLayout from 'containers/MainLayout'
 import AuthProvider from 'context/Authentication'
+import { MeQueryReturns, ME_QUERY } from 'modules/users'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ME_QUERY } from 'modules/users'
 import { PublicRoutes } from './PublicRoutes'
-import { useQuery } from '@apollo/client'
-import { MeQueryReturns } from 'modules/users'
 
-const Bootstrap: React.FC = () => {
+const Bootstrap: React.VFC = () => {
   const { loading, error, data } = useQuery<MeQueryReturns>(ME_QUERY)
 
   if (loading || data === undefined) return <span>Loading</span>
