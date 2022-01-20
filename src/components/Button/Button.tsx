d19a224d15bedd0df39c0f4c5b25b8c52a93196a
Alexander Orvik <alexao1111@gmail.com>
2022-01-20 22:40:55 +0100

    feat(componnets): rework basic Button component
    * Rename from PrimaryButton to Button
    * Add styling based on props 'style' enum value

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,50 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 interface ButtonProps {
   width?: string
   height?: string
+  borderRadius?: string
+  backgroundColor?: string
   type?: 'submit' | 'text' | 'reset'
+  buttonStyle?: 'primary' | 'cancel'
 
   className?: string
-  onClick: () => void
+  onClick?: () => void
 }
 
-const StyledButton = styled.button<ButtonProps>``
+const getButtonStyle = (buttonType: 'primary' | 'cancel' | undefined) => {
+  if (buttonType === undefined) return css``
+  switch (buttonType) {
+    case 'primary':
+      return css`
+        background-color: ${props => props.theme.colors.purpleAction};
+        color: ${props => props.theme.colors.white};
+      `
+    case 'cancel':
+      return css`
+        background-color: ${props => props.theme.colors.gray1};
+        color: ${props => props.theme.colors.white};
+      `
+  }
+}
 
-export const PrimaryButton: React.FC<ButtonProps> = ({
+const StyledButton = styled.button<ButtonProps>`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  font-weight: 500;
+  font-size: 14px;
+  border-radius: ${props => props.borderRadius};
+  border: none;
+  box-shadow: ${props => props.theme.shadow.default};
+  ${props => getButtonStyle(props.buttonStyle)};
+`
+
+export const Button: React.FC<ButtonProps> = ({
   width = '100%',
   height = '35px',
+  borderRadius = '4px',
+  backgroundColor = 'purple',
+  buttonStyle = 'primary',
   type,
   className,
   children,
@@ -24,8 +55,11 @@ export const PrimaryButton: React.FC<ButtonProps> = ({
     <StyledButton
       width={width}
       height={height}
+      borderRadius={borderRadius}
+      backgroundColor={backgroundColor}
       className={className}
       onClick={onClick}
+      buttonStyle={buttonStyle}
       // type={type}
     >
       {children}
