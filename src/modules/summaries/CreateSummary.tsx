c9f84038456616d88be8c7fc21879a46bca29873
Alexander Orvik <alexao1111@gmail.com>
2022-01-26 03:23:48 +0100

    refactor(summaries): Implement proper create summary styling

--- src/modules/summaries/CreateSummary.tsx
+++ src/modules/summaries/CreateSummary.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client'
 import { ErrorMessage } from '@hookform/error-message'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { Card } from 'components/Card'
 import { UserMultiSelect, UserSelect } from 'components/Select'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useHistory } from 'react-router-dom'
@@ -24,17 +25,81 @@ type SummaryInput = {
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   overflow-y: scroll;
   padding: 32px;
+
+  ${props => props.theme.media.mobile} {
+    padding: 16px;
+  }
 `
 
-const Title = styled.h1``
+const Title = styled.h1`
+  margin-bottom: 10px;
+`
+
+const FormTop = styled.div`
+  display: grid;
+  width: 100%;
+  height: auto;
+  grid-template-areas:
+    'title . . .'
+    'reporter reporter participants participants'
+    'type date . .';
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-row-gap: 10px;
+  grid-column-gap: 20px;
+  ${props => props.theme.media.mobile} {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+const ReporterContainer = styled.div`
+  grid-area: reporter;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const ParticipantsContainer = styled.div`
+  grid-area: participants;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const TypeContainer = styled.div`
+  grid-area: type;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const DateContainer = styled.div`
+  grid-area: date;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
 
 const TextArea = styled.textarea`
+  grid-area: content;
   width: 100%;
   height: 600px;
+  border-radius: 10px;
+  padding: 10px;
+  outline: none;
+  border: none;
+  margin: 15px 0;
+  box-shadow: ${props => props.theme.shadow.default};
 `
 
 export const CreateSummary = () => {
@@ -66,6 +131,7 @@ export const CreateSummary = () => {
     register,
     handleSubmit,
     setValue,
+    getValues,
     formState: { errors },
   } = useForm<SummaryInput>({
     resolver: yupResolver(schema),
@@ -95,24 +161,49 @@ export const CreateSummary = () => {
 
   return (
     <Wrapper>
-      <Title>Nytt referat</Title>
+      <Title>Rediger referat</Title>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        Referent:
-        <UserSelect setUserCallback={handleUpdateReporter} />
-        <UserMultiSelect setUsersCallback={handleUpdateParticipants} />
-        <ErrorMessage
-          errors={errors}
-          name="reporter"
-          render={({ message }) => <p>{message}</p>}
-        />
-        <select {...register('type')}>
-          {summaryTypeChoices.map((choice, i) => (
-            <option value={choice.value} key={i}>
-              {choice.label}
-            </option>
-          ))}
-        </select>
-        <input placeholder="YYYY-MM-DD" {...register('date')} />
+        <Card>
+          <FormTop>
+            <ReporterContainer>
+              <label>Referent</label>
+              <UserSelect
+                fullwidth
+                userId={getValues('reporter')}
+                setUserCallback={handleUpdateReporter}
+              />
+              <ErrorMessage
+                errors={errors}
+                name="reporter"
+                render={({ message }) => <p>{message}</p>}
+              />
+            </ReporterContainer>
+            <ParticipantsContainer>
+              <label>Deltakere</label>
+              <UserMultiSelect
+                fullwidth
+                users={getValues('participants')}
+                setUsersCallback={handleUpdateParticipants}
+              />
+            </ParticipantsContainer>
+            <TypeContainer>
+              <label>Type referat</label>
+              <select {...register('type')}>
+                {summaryTypeChoices.map((choice, i) => (
+                  <option value={choice.value} key={i}>
+                    {choice.label}
+                  </option>
+                ))}
+              </select>
+            </TypeContainer>
+
+            <DateContainer>
+              <label>Dato</label>
+              <input placeholder="YYYY-MM-DD" {...register('date')} />
+            </DateContainer>
+          </FormTop>
+        </Card>
+
         <TextArea {...register('contents')} />
         <ErrorMessage
           errors={errors}
