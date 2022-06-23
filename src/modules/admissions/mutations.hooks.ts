a6878988158fdd430f5107386c5a70bcd5ca13f4
Alexander Orvik <alexao1111@gmail.com>
2022-06-23 21:45:01 +0200

    feata(admisisons): add more mutation hooks

--- src/modules/admissions/mutations.hooks.ts
+++ src/modules/admissions/mutations.hooks.ts
@@ -104,7 +104,7 @@ interface CreateApplicantInterestVariables {
 
 const CREATE_APPLICANT_INTEREST = gql`
   mutation CreateApplicantInterest($input: CreateApplicantInterestInput!) {
-    createApplicantInput(input: $input) {
+    createApplicantInterest(input: $input) {
       applicantInterest {
         id
       }
@@ -140,4 +140,105 @@ export const useDeleteApplicantInterest = () => {
   >(DELETE_APPLICANT_INTEREST)
 
   return { deleteApplicantInterest, loading, error }
+}
+
+interface ToggleApplicantWillBeAdmittedMutationReturns {
+  toggleApplicantWillBeAdmitted: {
+    success: boolean
+  }
+}
+
+interface ToggleApplicantWillBeAdmittedMutationVariables {
+  id: string
+}
+
+const TOGGLE_APPLICANT_WILL_BE_ADMITTED_MUTATION = gql`
+  mutation ToggleApplicantWillBeAdmittedMutation($id: ID!) {
+    toggleApplicantWillBeAdmitted(id: $id) {
+      success
+    }
+  }
+`
+
+export const useToggleApplicantWillBeAdmitted = () => {
+  const [toggleApplicantWillBeAdmitted, { error, loading }] = useMutation<
+    ToggleApplicantWillBeAdmittedMutationReturns,
+    ToggleApplicantWillBeAdmittedMutationVariables
+  >(TOGGLE_APPLICANT_WILL_BE_ADMITTED_MUTATION)
+
+  return {
+    toggleApplicantWillBeAdmitted,
+    loading,
+    error,
+  }
+}
+
+const GIVE_APPLICANT_TO_INTERNAL_GROUP_MUTATION = gql`
+  mutation GiveApplicantToInternalGroup($applicantInterestId: ID!) {
+    giveApplicantToInternalGroup(applicantInterestId: $applicantInterestId) {
+      success
+    }
+  }
+`
+
+interface GiveApplicantToInternalGroupMutationReturns {
+  success: boolean
+}
+
+interface GiveApplicantToInternalGroupMutationVariables {
+  applicantInterestId: string
+}
+
+export const useGiveApplicantToInternalGroupMutation = () => {
+  const [giveApplicantToInternalGroupMutation, { error, loading }] =
+    useMutation<
+      GiveApplicantToInternalGroupMutationReturns,
+      GiveApplicantToInternalGroupMutationVariables
+    >(GIVE_APPLICANT_TO_INTERNAL_GROUP_MUTATION)
+
+  return {
+    giveApplicantToInternalGroupMutation,
+    error,
+    loading,
+  }
+}
+
+const RESET_APPLICANT_INTERNAL_GROUP_POSITION_OFFER = gql`
+  mutation ResetApplicantInternalGroupPositionOffer($applicantInterestId: ID!) {
+    resetApplicantInternalGroupPositionOffer(
+      applicantInterestId: $applicantInterestId
+    ) {
+      applicantInterest {
+        id
+        applicant {
+          id
+          fullName
+        }
+      }
+    }
+  }
+`
+
+interface ResetApplicantInternalGroupPositionOfferReturns {
+  applicantInterest: {
+    id: string
+    applicant: Pick<ApplicantNode, 'id' | 'fullName'>
+  }
+}
+interface ResetApplicantInternalGroupPositionOfferVariables {
+  applicantInterestId: string
+}
+
+export const useResetApplicantInternalGroupPositionOffer = () => {
+  const [resetApplicantInternalGroupPositionOfferMutation, { loading, error }] =
+    useMutation<
+      ResetApplicantInternalGroupPositionOfferReturns,
+      ResetApplicantInternalGroupPositionOfferVariables
+    >(RESET_APPLICANT_INTERNAL_GROUP_POSITION_OFFER)
+
+  return {
+    resetApplicantInternalGroupPositionOfferMutation,
+    loading,
+    error,
+  }
 }
