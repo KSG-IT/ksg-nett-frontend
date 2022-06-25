269f2cd3e6d61621d83481fa04755c1305dd2bd9
Alexander Orvik <alexao1111@gmail.com>
2022-06-25 18:51:32 +0200

    feat(admissions): add detail view redirect MyInterviews

--- src/modules/admissions/MyInterviews.tsx
+++ src/modules/admissions/MyInterviews.tsx
@@ -1,8 +1,10 @@
 import { useQuery } from '@apollo/client'
 import { Button, Paper, Stack, Table, Title } from '@mantine/core'
+import { FullPageError } from 'components/FullPageComponents'
 import { FullContentLoader } from 'components/Loading'
 import { format } from 'date-fns'
 import { gql } from 'graphql-tag'
+import { useHistory } from 'react-router-dom'
 import { InterviewNode } from './types'
 
 interface MyInterviewsReturns {
@@ -34,24 +36,36 @@ const useMyInterviews = () => {
 
 export const MyInterviews: React.VFC = () => {
   const { error, data, loading } = useMyInterviews()
+  const history = useHistory()
+
+  if (error) return <FullPageError />
 
   if (loading || !data) return <FullContentLoader />
-  console.log(data)
 
   const { myInterviews } = data
+
+  const handleRedirectToInterview = (applicantId: string) => {
+    history.push(`/admissions/applicants/${applicantId}`)
+  }
 
   const rows = myInterviews.map(interview => (
     <tr>
       <td>{interview.applicant.fullName}</td>
       <td>{format(new Date(interview.interviewStart), 'iii d MMM HH:mm')}</td>
       <td>
-        <Button>Detaljer</Button>
+        <Button
+          onClick={() => {
+            handleRedirectToInterview(interview.applicant.id)
+          }}
+        >
+          Detaljer
+        </Button>
       </td>
     </tr>
   ))
 
   return (
-    <Stack>
+    <Stack style={{ overflowY: 'scroll', width: '100%' }} p="lg">
       <Title>Mine intervjuer</Title>
       <Paper p="md">
         <Table>
