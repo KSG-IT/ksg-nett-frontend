82cff693911512c56e70aa52c5cbc9cc8e542962
Alexander Orvik <alexao1111@gmail.com>
2022-08-03 08:27:26 +0200

    feat: add deployment shell-script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" = '' ]
+then
+	echo "Please specify an environment"
+fi
+
+
+if [ "$1" = 'development' ]
+then
+	aws s3 sync --acl public-read dist/ s3://app.ksg-nett.no
+
+	aws cloudfront create-invalidation --distribution-id E2K4WV02BTO7FQ --paths "/*"
+	exit
+fi
