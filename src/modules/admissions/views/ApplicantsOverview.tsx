89a0ac7ccbf4d2b3b8557462b4aa199575ba8f81
Alexander Orvik <alexao1111@gmail.com>
2022-08-10 19:49:44 +0200

    feat(admissions): ApplicantOverview.view add applicant component

--- src/modules/admissions/views/ApplicantsOverview.tsx
+++ src/modules/admissions/views/ApplicantsOverview.tsx
@@ -1,40 +1,20 @@
 import { useQuery } from '@apollo/client'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button, ScrollArea, Stack, Textarea, Title } from '@mantine/core'
+import { ScrollArea, Stack, Title } from '@mantine/core'
 import { FullPageError } from 'components/FullPageComponents'
 import { FullContentLoader } from 'components/Loading'
-import { MessageBox } from 'components/MessageBox'
 import { PermissionGate } from 'components/PermissionGate'
-import { useState } from 'react'
-import toast from 'react-hot-toast'
 import { ApplicantsTable } from '../components/ApplicantsOverview'
-import { useApplicantMutations } from '../mutations.hooks'
+import { AddApplicantsArea } from '../components/ApplicantsOverview/AddApplicantsArea'
 import { CURRENT_APPLICANTS_QUERY } from '../queries'
 import { CurrentApplicantsReturns } from '../types.graphql'
 
 export const ApplicantsOverview: React.FC<{}> = ({}) => {
-  const [emails, setEmails] = useState('')
   const { data, loading, error } = useQuery<CurrentApplicantsReturns>(
     CURRENT_APPLICANTS_QUERY,
     {
       pollInterval: 20000,
     }
   )
-
-  const { createApplicants, createApplicantsLoading } = useApplicantMutations()
-
-  const handleCreateApplicants = () => {
-    const parsedEmails = emails
-      .split('\n')
-      .filter(emailString => emailString !== '')
-
-    toast.promise(createApplicants({ variables: { emails: parsedEmails } }), {
-      loading: 'Oppretter søknader',
-      error: 'Noe gikk galt',
-      success: 'søknader opprettet',
-    })
-    setEmails('')
-  }
 
   if (error) return <FullPageError />
 
@@ -45,28 +25,9 @@ export const ApplicantsOverview: React.FC<{}> = ({}) => {
   return (
     <ScrollArea p="md" style={{ width: '100%' }}>
       <Stack>
-        <Title mb="lg">Søkeroversikt</Title>
+        <Title>Søkeroversikt</Title>
         <PermissionGate permissions={['admissions.add_applicant']}>
-          <Title mt="md" mb="md" order={2}>
-            Legg til søkere
-          </Title>
-          <MessageBox type="info">
-            Her kan du legge inn søkere sin epost. Hver epost på hver sin linje.
-          </MessageBox>
-          <Textarea
-            minRows={12}
-            placeholder="søker1@epost.com&#10;søker2@epost.com&#10;..."
-            value={emails}
-            onChange={e => setEmails(e.target.value)}
-          />
-          <Button
-            style={{ marginTop: '1rem' }}
-            leftIcon={<FontAwesomeIcon icon="paper-plane" />}
-            onClick={handleCreateApplicants}
-            disabled={createApplicantsLoading}
-          >
-            Legg til søkere
-          </Button>
+          <AddApplicantsArea />
         </PermissionGate>
         <ApplicantsTable applicants={currentApplicants} />
       </Stack>
