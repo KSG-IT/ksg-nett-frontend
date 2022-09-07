300aab2cc1e220023f6ebf5f3cf46e4e0c0105c5
plebbimon <sebastiansmaland@gmail.com>
2022-09-08 00:40:40 +0200

    feat: Users history and quotes

---
+++ src/modules/users/components/UserHistory.tsx
@@ -0,0 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Text, ThemeIcon, Timeline } from '@mantine/core'
+import { InternalGroupPositionMembershipNode } from '../UserManagement/types'
+
+interface UserHistoryProps {
+  memberships: InternalGroupPositionMembershipNode[]
+}
+
+export function UserHistory({ memberships }: UserHistoryProps) {
+  const fields = memberships.map(
+    ({ id, position, membershipStart, membershipEnd }) => (
+      <Timeline.Item
+        bulletSize={37}
+        bullet={
+          <ThemeIcon
+            size={35}
+            variant="gradient"
+            gradient={{ from: 'orange', to: 'gold' }}
+            radius="xl"
+          >
+            <FontAwesomeIcon icon={['fas', 'coffee']} />
+          </ThemeIcon>
+        }
+        key={id}
+        title={position.name}
+      >
+        <Text color={'dimmed'} size={'sm'}>
+          {position.internalGroup.name}
+        </Text>
+        <Text size={'sm'}>
+          {membershipStart} {membershipEnd && ` - ${membershipEnd}`}
+        </Text>
+      </Timeline.Item>
+    )
+  )
+
+  return <Timeline>{fields}</Timeline>
+}
---
+++ src/modules/users/components/UserQuotes.tsx
@@ -0,0 +1,38 @@
+import { Avatar, Card, createStyles, SimpleGrid, Text } from '@mantine/core'
+import { QuoteNode } from 'modules/quotes/types'
+import { UserThumbnail } from './UserThumbnail'
+
+interface UserQuotesProps {
+  quotes: QuoteNode[]
+}
+
+const useStyles = createStyles(theme => ({
+  quoteText: {
+    color: theme.colors.gray[7],
+    fontStyle: 'italic',
+  },
+  card: {
+    maxWidth: 400,
+  },
+}))
+
+export function UserQuotes({ quotes }: UserQuotesProps) {
+  const { classes } = useStyles()
+  const fields = quotes.map(quote => (
+    <Card className={classes.card} key={quote.id} radius="lg" shadow={'xs'}>
+      <Card.Section p={'md'}>
+        <Text className={classes.quoteText}>{quote.text}</Text>
+        <Avatar.Group pt={'xs'}>
+          {quote.tagged.map(user => (
+            <UserThumbnail key={user.id} user={user} />
+          ))}
+        </Avatar.Group>
+      </Card.Section>
+    </Card>
+  ))
+  return (
+    <SimpleGrid cols={2} p="md">
+      {fields}
+    </SimpleGrid>
+  )
+}
