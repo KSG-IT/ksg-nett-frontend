a7fb6320cfe7dea5c4ed984f8c7f4e0dccd877b6
plebbimon <sebastiansmaland@gmail.com>
2022-10-12 17:55:23 +0200

    refactor: added :tabValue route for quotes not even sure if it works at all

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -26,6 +26,7 @@ import {
   QuotesList,
   ReviewQuotes,
 } from 'modules/quotes'
+import { QuotesTabs } from 'modules/quotes/components/QuotesTabs'
 import {
   AllMyShifts,
   MyUpcomingShifts,
@@ -100,6 +101,8 @@ export const AppRoutes: React.FC = () => {
         </Route>
 
         <Route path="quotes">
+          <Route path=":tabValue" element={<QuotesTabs />} />
+          <Route path="popular" element={<PopularQuotes />} />
           <Route index element={<QuotesList />} />
           <Route
             path="review"
