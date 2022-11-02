659bfeedd305bd442f39cb9dfc1029914726bd6d
Alexander Orvik <alexao1111@gmail.com>
2022-11-02 14:06:20 +0100

    feat(quotes): improve quote search
    	* Move querying to subcomponent reducing input lag
    	forr search term

--- src/modules/quotes/views/QuotesList.tsx
+++ src/modules/quotes/views/QuotesList.tsx
@@ -1,24 +1,10 @@
-import { useQuery } from '@apollo/client'
-import {
-  Button,
-  Container,
-  Group,
-  SimpleGrid,
-  Stack,
-  Title,
-} from '@mantine/core'
+import { Group, Stack, Title } from '@mantine/core'
 import { Breadcrumbs } from 'components/Breadcrumbs'
 import { Search } from 'components/Input'
 import { useState } from 'react'
-import { DEFAULT_PAGINATION_SIZE } from 'util/consts'
 import { useDebounce } from 'util/hooks/useDebounce'
-import { QuoteCard } from '../components/QuoteCard'
+import { QuoteGrid } from '../components/QuoteGrid'
 import { QuotesTabs } from '../components/QuotesTabs'
-import { APPROVED_QUOTES_QUERY } from '../queries'
-import {
-  ApprovedQuotesReturns,
-  ApprovedQuotesVariables,
-} from '../types.graphql'
 
 const breadCrumbItems = [
   { label: 'Hjem', path: '/dashboard' },
@@ -28,50 +14,6 @@ const breadCrumbItems = [
 export const QuotesList = () => {
   const [query, setQuery] = useState('')
   const debouncedQuery = useDebounce(query)
-
-  // Move query to own component that executes query and loading to get rid of lag
-  const { data, fetchMore, loading } = useQuery<
-    ApprovedQuotesReturns,
-    ApprovedQuotesVariables
-  >(APPROVED_QUOTES_QUERY, {
-    variables: { q: debouncedQuery, first: DEFAULT_PAGINATION_SIZE },
-  })
-
-  const quotes = data?.approvedQuotes.edges.map(edge => edge.node) ?? []
-  const hasNextPage = data?.approvedQuotes.pageInfo.hasNextPage ?? false
-
-  const handleFetchMore = async () => {
-    if (typeof data === 'undefined') return
-
-    try {
-      await fetchMore({
-        variables: {
-          first: DEFAULT_PAGINATION_SIZE,
-          q: debouncedQuery,
-          after: data.approvedQuotes.pageInfo.endCursor,
-        },
-        updateQuery(prev, { fetchMoreResult }) {
-          const newQuotes = fetchMoreResult?.approvedQuotes
-          if (newQuotes === undefined) return prev
-
-          const newData = {
-            approvedQuotes: {
-              ...prev.approvedQuotes,
-              pageInfo: newQuotes.pageInfo,
-              edges: [...prev.approvedQuotes.edges, ...newQuotes.edges],
-            },
-          }
-          return newData
-        },
-      })
-    } catch (error) {
-      // This is a thing https://stackoverflow.com/questions/68240884/error-object-inside-catch-is-of-type-unkown
-      if (!(error instanceof Error)) return
-      if (error.name === 'Invariant Violation') return
-
-      throw error
-    }
-  }
 
   return (
     <Stack>
@@ -88,28 +30,7 @@ export const QuotesList = () => {
         />
         <QuotesTabs />
       </Group>
-      <SimpleGrid
-        cols={4}
-        breakpoints={[
-          { maxWidth: 'lg', cols: 3, spacing: 'md' },
-          { maxWidth: 'sm', cols: 1, spacing: 'sm' },
-        ]}
-      >
-        {quotes.map(quote => (
-          <QuoteCard quote={quote} key={quote.id} displaySemester />
-        ))}
-      </SimpleGrid>
-      <Container>
-        {hasNextPage && (
-          <Button
-            color="samfundet-red"
-            loading={loading}
-            onClick={handleFetchMore}
-          >
-            Hent flere sitater
-          </Button>
-        )}
-      </Container>
+      <QuoteGrid search={debouncedQuery} />
     </Stack>
   )
 }
