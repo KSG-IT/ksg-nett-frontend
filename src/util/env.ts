378308d032068d9784aae85c2f8f16bdbe285681
Alexander Orvik <alexao1111@gmail.com>
2022-11-04 11:51:10 +0100

    fix(env): env api url reference

--- src/util/env.ts
+++ src/util/env.ts
@@ -4,4 +4,4 @@ export const MEDIA_URL = import.meta.env.PROD
   ? ''
   : 'http://localhost:8000/media/'
 
-export const API_URL = import.meta.env.VITE_APP_API_URL
+export const API_URL = import.meta.env.VITE_API_URL
