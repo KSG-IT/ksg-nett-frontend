947d90762a9c5efa9ffa0ee5e419a71cbea67181
Alexander Orvik <alexao1111@gmail.com>
2022-11-18 15:34:22 +0100

    feat(deployment): update depoyment with production settings

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,15 @@ if [ "$1" = 'development' ]
 then
 	aws s3 sync --acl public-read dist/ s3://app.ksg-nett.no
 
+	aws cloudfront create-invalidation --distribution-id E16GL36WMCPKCU --paths "/*"
+	exit
+fi
+
+
+if [ "$1" = 'production' ]
+then
+	aws s3 sync --acl public-read dist/ s3://app-dev.ksg-nett.no
+
 	aws cloudfront create-invalidation --distribution-id E2K4WV02BTO7FQ --paths "/*"
 	exit
 fi
