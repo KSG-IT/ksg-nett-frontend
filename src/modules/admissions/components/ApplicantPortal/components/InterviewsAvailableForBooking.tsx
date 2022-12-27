4b016accd37ed0cb2edbf88d29a319e7896bff50
plebbimon <sebastiansmaland@gmail.com>
2022-12-27 15:21:42 +0100

    refactor: priorities and interview booking Priority changes now reflect to backend, and also animates Mobile view is working with minor layout issues Interview Booking has gotten some small redesigns

---
+++ src/modules/admissions/components/ApplicantPortal/components/InterviewsAvailableForBooking.tsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Container,
+  Group,
+  Stack,
+  Title,
+  Text,
+  SimpleGrid,
+  Divider,
+} from '@mantine/core'
+import { format } from '../../../../../util/date-fns'
+import {
+  AvailableInterviewsDayGrouping,
+  InterviewsAvailableForBookingReturns,
+} from '../../../types.graphql'
+
+export interface InterviewsAvailableProps {
+  interviews: AvailableInterviewsDayGrouping[]
+  handleCallback: (interviewIds: string[]) => void
+}
+
+export const InterviewsAvailableForBooking: React.FC<
+  InterviewsAvailableProps
+> = ({ interviews, handleCallback }) => {
+  if (interviews.length === 0) {
+    return (
+      <Container>
+        <Title order={3}>Ingen intervjuer tilgjengelig</Title>
+        <Text>
+          Det er ingen intervjuer tilgjengelig for booking. Prøv igjen senere
+          eller sjekk andre dager.
+        </Text>
+      </Container>
+    )
+  }
+  return (
+    <>
+      <Title color={'dimmed'} order={3}>
+        Tilgjengelige tidspunkter
+      </Title>
+      <Divider my={'xs'} />
+      {interviews.map(interviewDay => (
+        <Stack key={`${interviewDay.date}`}>
+          <Title order={5} transform={'uppercase'}>
+            {format(new Date(interviewDay.date), 'EEEE dd MMM')}
+          </Title>
+          <SimpleGrid
+            cols={6}
+            breakpoints={[{ maxWidth: 600, cols: 1, spacing: 'sm' }]}
+          >
+            {interviewDay.interviewSlots.map((slot, i) => {
+              const unavailable = slot.interviewIds.length === 0
+              return (
+                <Button
+                  radius={'md'}
+                  key={i}
+                  disabled={unavailable}
+                  onClick={() => {
+                    handleCallback(slot.interviewIds)
+                  }}
+                >
+                  {format(new Date(slot.interviewStart), 'HH:mm')}
+                </Button>
+              )
+            })}
+          </SimpleGrid>
+        </Stack>
+      ))}
+      <Divider my={'xs'} />
+    </>
+  )
+}
