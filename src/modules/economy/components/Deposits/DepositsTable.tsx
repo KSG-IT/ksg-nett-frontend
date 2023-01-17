3905d665aa3cc5e39cf6b776b65cd52f8e7ac52e
Alexander Orvik <alexao1111@gmail.com>
2023-01-17 17:20:54 +0100

    refactor(economy): deposit receipt view

--- src/modules/economy/components/Deposits/DepositsTable.tsx
+++ src/modules/economy/components/Deposits/DepositsTable.tsx
@@ -1,4 +1,5 @@
-import { Button, createStyles, Image, Modal, Paper, Table } from '@mantine/core'
+import { Button } from '@mantine/core'
+import { showNotification } from '@mantine/notifications'
 import { CardTable } from 'components/CardTable'
 import { FullContentLoader } from 'components/Loading'
 import { useDepositMutations } from 'modules/economy/mutations.hooks'
@@ -6,8 +7,6 @@ import { ALL_DEPOSITS } from 'modules/economy/queries'
 import { DepositNode } from 'modules/economy/types.graphql'
 import { UserThumbnail } from 'modules/users/components'
 import { ME_QUERY } from 'modules/users/queries'
-import { useState } from 'react'
-import toast from 'react-hot-toast'
 import { format } from 'util/date-fns'
 
 interface DepositsTableProps {
@@ -19,32 +18,13 @@ export const DepositsTable: React.FC<DepositsTableProps> = ({
   deposits,
   queryLoading,
 }) => {
-  const { classes } = useDepositsTableStyles()
-  const [previewModalOpen, setPreviewModalOpen] = useState(false)
-  const [previewImageIndex, setPreviewImageIndex] = useState<number | null>(
-    null
-  )
-
   const { approveDeposit, invalidateDeposit } = useDepositMutations()
 
   if (queryLoading) return <FullContentLoader />
 
-  function handlePreviewImage(index: number) {
-    setPreviewImageIndex(index)
-    setPreviewModalOpen(true)
-  }
-
-  function getPreviewImage(): string {
-    if (previewImageIndex === null) return ''
-
-    const receipt = deposits[previewImageIndex]?.receipt
-    if (receipt === null || receipt === undefined) return ''
-
-    return receipt
-  }
-
   function handleDeposit(deposit: DepositNode) {
     const handler = deposit.approved ? invalidateDeposit : approveDeposit
+
     const message = deposit.approved
       ? 'Innskudd underkjent'
       : 'Innskudd godkjent'
@@ -54,11 +34,18 @@ export const DepositsTable: React.FC<DepositsTableProps> = ({
         depositId: deposit.id,
       },
       refetchQueries: [ALL_DEPOSITS, ME_QUERY],
-      onError() {
-        toast.error('Noe gikk galt')
-      },
       onCompleted() {
-        toast.success(message)
+        showNotification({
+          title: 'Suksess',
+          color: 'teal',
+          message,
+        })
+      },
+      onError({ message }) {
+        showNotification({
+          title: 'Noe gikk galt',
+          message,
+        })
       },
     })
   }
@@ -69,13 +56,9 @@ export const DepositsTable: React.FC<DepositsTableProps> = ({
       <td>{deposit.account.user.fullName}</td>
       <td>{deposit.amount}</td>
       <td>
-        <Button
-          variant="subtle"
-          color="samfundet-red"
-          onClick={() => handlePreviewImage(index)}
-        >
+        <a href={deposit.receipt || ''} target="_blank">
           Se kvittering
-        </Button>
+        </a>
       </td>
       <td>
         {deposit.approvedBy ? (
@@ -97,40 +80,18 @@ export const DepositsTable: React.FC<DepositsTableProps> = ({
   ))
 
   return (
-    <>
-      <CardTable>
-        <thead>
-          <tr>
-            <th>Tidsstempel</th>
-            <th>Navn</th>
-            <th>Sum</th>
-            <th>Kvittering</th>
-            <th>Godkjent av</th>
-            <th>Handling</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </CardTable>
-      <Modal
-        fullScreen
-        title="Kvittering"
-        opened={previewModalOpen}
-        onClose={() => setPreviewModalOpen(false)}
-      >
-        {previewImageIndex !== null && (
-          <Image className={classes.previewImage} src={getPreviewImage()} />
-        )}
-      </Modal>
-    </>
+    <CardTable>
+      <thead>
+        <tr>
+          <th>Tidsstempel</th>
+          <th>Navn</th>
+          <th>Sum</th>
+          <th>Kvittering</th>
+          <th>Godkjent av</th>
+          <th>Handling</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </CardTable>
   )
 }
-
-const useDepositsTableStyles = createStyles(theme => ({
-  previewImage: {
-    width: '100%',
-    height: '100%',
-  },
-  paper: {
-    overflowX: 'scroll',
-  },
-}))
