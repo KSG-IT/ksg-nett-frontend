ad75d9a7353e7b6de0cd48942747d67d63fe777d
Alexander Orvik <alexao1111@gmail.com>
2023-01-17 01:17:41 +0100

    feat(schedules): Set date using url param todays shifts

--- src/modules/schedules/views/AllShifts.tsx
+++ src/modules/schedules/views/AllShifts.tsx
@@ -5,7 +5,8 @@ import { Breadcrumbs } from 'components/Breadcrumbs'
 import { FullPageError } from 'components/FullPageComponents'
 import { FullContentLoader } from 'components/Loading'
 import { format } from 'date-fns'
-import { useState } from 'react'
+import queryString from 'query-string'
+import { useEffect, useRef, useState } from 'react'
 import { UserShiftCardList } from '../components'
 import { ALL_SHIFTS } from '../queries'
 import { AllShiftsReturns, AllShiftsVariables } from '../types.graphql'
@@ -18,6 +19,7 @@ const breadcrumbsItems = [
 export const AllShifts = () => {
   const [date, setDate] = useState<Date>(new Date())
   const { classes } = useAllShiftsStyles()
+  const firstRender = useRef(true)
 
   const { data, error, loading } = useQuery<
     AllShiftsReturns,
@@ -27,17 +29,49 @@ export const AllShifts = () => {
     pollInterval: 30_000,
   })
 
+  useEffect(() => {
+    // Check if there is a date in the query string. If not use today's date and update the query string
+    if (!firstRender.current) return
+    firstRender.current = false
+
+    console.log(location.search)
+
+    const search = queryString.parse(location.search)
+    const dateString = search.date as string
+
+    if (dateString) {
+      const date = new Date(dateString)
+      setDate(date)
+    } else {
+      setDate(new Date())
+      history.pushState(
+        {},
+        '',
+        `${location.pathname}?date=${format(new Date(), 'yyyy-MM-dd')}`
+      )
+    }
+  }, [setDate])
+
   if (error) return <FullPageError />
 
   if (loading || !data) return <FullContentLoader />
 
   const { allShifts } = data
 
+  function handleDateChange(date: Date) {
+    setDate(date)
+    history.pushState(
+      {},
+      '',
+      `/schedules/all-shifts?date=${format(date, 'yyyy-MM-dd')}`
+    )
+  }
+
   return (
     <div className={classes.wrapper}>
       <Breadcrumbs items={breadcrumbsItems} />
       <Title>Hva skjer'a?</Title>
-      <DatePicker value={date} onChange={val => val && setDate(val)} />
+      <DatePicker value={date} onChange={val => val && handleDateChange(val)} />
 
       <UserShiftCardList shifts={allShifts} />
     </div>
