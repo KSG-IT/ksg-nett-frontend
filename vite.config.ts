5305702f2fdd3e197261bb71153419eaf3b0e027
Alexander Orvik <alexao1111@gmail.com>
2023-01-20 15:41:15 +0100

    chore(sentry): revert sentry vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -10,24 +10,7 @@ dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    sentryVitePlugin({
-      org: 'ksg-it',
-      project: 'ksg-nett-frontend',
-
-      // Specify the directory containing build artifacts
-      include: './dist',
-
-      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-      // and needs the `project:releases` and `org:read` scopes
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-
-      // Optionally uncomment the line below to override automatic release name detection
-      // release: process.env.RELEASE,
-    }),
-  ],
+  plugins: [react(), tsconfigPaths(), ,],
   esbuild: {
     // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
     logOverride: { 'this-is-undefined-in-esm': 'silent' },
@@ -39,7 +22,6 @@ export default defineConfig({
     'process.env': {},
   },
   build: {
-    sourcemap: true,
     rollupOptions: {
       plugins: [nodePolyfills(), builtins(), globals()],
     },
