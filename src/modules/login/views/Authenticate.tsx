6bfe7d7b5e4b30e015ae6dfaa0c8e2b8bb8c55ae
Alexander Orvik <alexao1111@gmail.com>
2023-05-06 14:34:01 +0200

    feat(login): authenticate route improvements
    	* Login on mount instead of by button
    	* Add error boundary in case broken token

--- src/modules/login/views/Authenticate.tsx
+++ src/modules/login/views/Authenticate.tsx
@@ -1,8 +1,8 @@
-import { Button, Center } from '@mantine/core'
+import { Center, Loader } from '@mantine/core'
+import { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { setLoginToken } from 'util/auth'
+import { getLoginToken, setLoginToken } from 'util/auth'
 import { useJwtTokenFromQueryString } from '../hooks'
-import { getLoginToken } from 'util/auth'
 
 export const Authenticate: React.FC = () => {
   /**
@@ -13,6 +13,18 @@ export const Authenticate: React.FC = () => {
 
   const { token } = useJwtTokenFromQueryString()
   const navigate = useNavigate()
+  const firstRender = useRef(true)
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false
+      return
+    }
+
+    if (token) {
+      handleLogin()
+    }
+  }, [token])
 
   if (!token) {
     return <div>Oida, her gikk noe galt</div>
@@ -28,7 +40,7 @@ export const Authenticate: React.FC = () => {
 
   return (
     <Center>
-      <Button onClick={handleLogin}>Logg inn</Button>
+      <Loader />
     </Center>
   )
 }
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -6,6 +6,7 @@ import {
   Login,
   Authenticate,
 } from 'modules/login/views'
+import { ErrorBoundary } from 'react-error-boundary'
 import { Route, Routes } from 'react-router-dom'
 
 const PublicRoutes: React.FC = () => {
@@ -19,7 +20,18 @@ const PublicRoutes: React.FC = () => {
       <Route path="reset-password" element={<ChangePasswordWithToken />} />
 
       {/* authenticate endpoint is used to load a jwt token sent from backend emails to log in */}
-      <Route path="authenticate" element={<Authenticate />} />
+
+      <Route
+        path="authenticate"
+        element={
+          <ErrorBoundary
+            FallbackComponent={() => <div>Oida, her gikk noe galt</div>}
+          >
+            <Authenticate />
+          </ErrorBoundary>
+        }
+      />
+
       <Route path="login" element={<Login />} />
       <Route path="*" element={<Login />} />
     </Routes>
