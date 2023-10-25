a3f3946a14ef08fac583ff9c5f19e3ac7df5657a
Sven Herman Holmsen <svenhermanholmsen@gmail.com>
2023-10-25 14:05:27 +0200

    env change for local running

--- src/util/env.ts
+++ src/util/env.ts
@@ -4,4 +4,5 @@ export const MEDIA_URL = import.meta.env.PROD
   ? ''
   : 'http://localhost:8000/media/'
 
-export const API_URL = import.meta.env.VITE_API_URL
+export const API_URL = 'http://localhost:8000'
+//import.meta.env.VITE_API_URL
