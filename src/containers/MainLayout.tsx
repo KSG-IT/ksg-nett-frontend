b5b01af71c6548d8887e99497fb949985dbbf4e2
Maiken Gjerdseth <magjerds@ntnu.no>
2024-02-01 19:44:06 +0100

    feat(randomizelogo): Added different logoes and rotation of logo display

--- src/containers/MainLayout.tsx
+++ src/containers/MainLayout.tsx
@@ -18,7 +18,15 @@ import React, { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Link } from 'react-router-dom'
 import { useSidebar } from 'util/hooks'
-import logoUrl from '../assets/images/548spaghetti_100786.png'
+//import logoes
+import logoUrl0 from '../assets/images/548spaghetti_100786.png'
+import logoUrl1 from '../assets/images/ksglogo1.png'
+import logoUrl2 from '../assets/images/ksglogo2.png'
+import logoUrl3 from '../assets/images/ksglogo3.png'
+import logoUrl4 from '../assets/images/ksglogo4.png'
+import logoUrl5 from '../assets/images/ksglogo5.png'
+import logoUrl6 from '../assets/images/ksglogo6.png'
+import logoUrl7 from '../assets/images/ksglogo7.png'
 import { AppNavbar } from './Navbar'
 import { WhatsNewNotification } from 'components/WhatsNewNotification'
 interface ErrorFallbackProps {
@@ -47,6 +55,28 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const theme = useMantineTheme()
   const { sidebarOpen, toggleSidebar } = useSidebar()
   const { classes } = useStyles()
+  //Randomize logo logic:
+  const logoes = [
+    logoUrl1,
+    logoUrl2,
+    logoUrl3,
+    logoUrl4,
+    logoUrl5,
+    logoUrl6,
+    logoUrl7,
+  ]
+  function randomizeLogo() {
+    const index = Math.floor(Math.random() * logoes.length)
+    return logoes[index]
+  }
+  const [logoUrl, setLogoUrl] = React.useState(logoes[0])
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLogoUrl(randomizeLogo()) // Update logoUrl every 5 seconds
+    }, 5000) // 5000 milliseconds = 5 seconds
+
+    return () => clearInterval(intervalId) // Cleanup the interval on component unmount
+  }, [])
 
   return (
     <AppShell
@@ -80,7 +110,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
               <MediaQuery smallerThan={'md'} styles={{ display: 'none' }}>
                 <Group>
                   <Link to="/dashboard">
-                    <Image src={logoUrl} width={48} height={48} />
+                    <Image src={logoUrl} width={70} height={70} />
                   </Link>
                   <Link to="/dashboard">
                     <Text weight={700} size="lg">
