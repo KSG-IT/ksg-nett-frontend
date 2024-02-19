e4be76c9080d0440be3d4d7f8b04ca5893fc309e
Sven Herman Holmsen <svenhermanholmsen@gmail.com>
2024-02-19 17:54:20 +0100

    fix: state for advanced search

--- src/modules/advancedSearch/components/UserCard.tsx
+++ src/modules/advancedSearch/components/UserCard.tsx
@@ -1,46 +1,17 @@
-import {
-  Avatar,
-  Badge,
-  Card,
-  createStyles,
-  Group,
-  Menu,
-  Stack,
-  Text,
-  UnstyledButton,
-  useMantineTheme,
-} from '@mantine/core'
-import { IconArrowBackUp, IconHash, IconTrash } from '@tabler/icons-react'
-import { PermissionGate } from 'components/PermissionGate'
+import { Image, Card, createStyles, Stack, Text } from '@mantine/core'
 
-import { UserThumbnail } from 'modules/users/components'
-import { ME_QUERY, USER_QUERY } from 'modules/users/queries'
-import { useState } from 'react'
 import { useStore } from 'store'
-import { PERMISSIONS } from 'util/permissions'
-import { useQuoteMutations } from '../mutations.hooks'
-import {
-  APPROVED_QUOTES_QUERY,
-  PNEDING_QUOTES_QUERY,
-  POPULAR_QUOTES_QUERY,
-} from '../queries'
-import { QuoteNode } from '../types.graphql'
+import { UserNode } from 'modules/users/types'
 
 interface UserCardProps {
-  user: Pick<
-    QuoteNode,
-    'text' | 'id' | 'tagged' | 'context' | 'sum' | 'semester'
-  >
+  user: Pick<UserNode, 'id' | 'firstName' | 'lastName' | 'profileImage'>
   displaySemester?: boolean
 }
-export const UserCard: React.FC<UserCardProps> = ({ user }) => {
+export const UserCard: React.FC<UserCardProps> = ({
+  user,
+  displaySemester,
+}) => {
   const { classes } = useStyles()
-  const refetchQueries = [
-    POPULAR_QUOTES_QUERY,
-    APPROVED_QUOTES_QUERY,
-    ME_QUERY,
-    USER_QUERY,
-  ]
 
   const me = useStore(state => state.user)!
 
@@ -48,63 +19,26 @@ export const UserCard: React.FC<UserCardProps> = ({ user }) => {
     <Card radius={'md'} className={classes.card} key={user.id} withBorder>
       <Stack justify={'space-between'} spacing={'xs'} className={classes.card}>
         <Stack spacing={'xs'}>
-          <Text size={'sm'} className={classes.quoteText}>
-            {quote.text}
+          <Card.Section>
+            <Image
+              src={user.profileImage}
+              height={200}
+              alt={'Profile picture of ' + user.firstName + ' ' + user.lastName}
+            />
+          </Card.Section>
+          <Text size={'sm'} className={classes.nameText}>
+            {user.firstName + ' ' + user.lastName}
           </Text>
-
-          <Text size={'xs'} color={'gray'}>
-            {quote.context}
-          </Text>
+          <Text>{displaySemester}</Text>
         </Stack>
-        <Group position="apart" spacing={'xs'}>
-          <Avatar.Group spacing={5}>
-            {quote.tagged.map(user => (
-              <UserThumbnail size={'sm'} key={user.id} user={user} />
-            ))}
-          </Avatar.Group>
-
-          <Group spacing={'xs'}>
-            <PermissionGate permissions={PERMISSIONS.quotes.invalidate.quote}>
-              <Menu>
-                <Menu.Target>
-                  <UnstyledButton>
-                    <IconHash size={18} color="lightgray" />
-                  </UnstyledButton>
-                </Menu.Target>
-                <Menu.Dropdown style={{ zIndex: 9000 }}>
-                  <PermissionGate
-                    permissions={PERMISSIONS.quotes.invalidate.quote}
-                  >
-                    <Menu.Item
-                      icon={<IconArrowBackUp />}
-                      onClick={handleInvalidateQuote}
-                    >
-                      Underkjenn
-                    </Menu.Item>
-                  </PermissionGate>
-                  <PermissionGate permissions={PERMISSIONS.quotes.delete.quote}>
-                    <Menu.Item icon={<IconTrash />} onClick={handleDeleteQuote}>
-                      Slett
-                    </Menu.Item>
-                  </PermissionGate>
-                </Menu.Dropdown>
-              </Menu>
-            </PermissionGate>
-            {displaySemester && (
-              <Badge variant="outline" color="samfundet-red">
-                {quote.semester}
-              </Badge>
-            )}
-          </Group>
-        </Group>
       </Stack>
     </Card>
   )
 }
 
 const useStyles = createStyles(theme => ({
-  quoteText: {
-    color: theme.colors.gray[7],
+  nameText: {
+    color: theme.colors.dark,
     fontWeight: 500,
   },
   card: {
--- src/modules/advancedSearch/view/AdvancedSearch.tsx
+++ src/modules/advancedSearch/view/AdvancedSearch.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 import {
-  Button,
   Checkbox,
   Group,
-  Input,
   Radio,
   Stack,
   Title,
@@ -12,8 +10,6 @@ import {
   TextInput,
 } from '@mantine/core'
 import { Breadcrumbs } from 'components/Breadcrumbs'
-import { LinkProps } from 'react-router-dom'
-import { useQuery } from '@apollo/client'
 import { useDebouncedValue } from '@mantine/hooks'
 import { IconSearch } from '@tabler/icons-react'
 import { UserGrid } from '../components/UserGrid'
@@ -24,34 +20,47 @@ const breadcrumbs = [
 ]
 
 export const AdvancedSearch = () => {
+  // Individual states for each search parameter
   const [query, setQuery] = useState('')
-  const [debouncedQuery] = useDebouncedValue(query, 200)
-  /**
-   * For reference from QuotesLlist.tsx:
-   * This view lags like crazy on search. Fix at some point
-   */
+  const [status, setStatus] = useState<string[]>([])
+  const [gang, setGang] = useState<string[]>([])
+  const [verv, setVerv] = useState<string[]>([])
+  const [konto, setKonto] = useState('')
+
+  const handleCheckboxChange = (
+    setState: React.Dispatch<React.SetStateAction<string[]>>,
+    value: string
+  ) => {
+    setState(prev =>
+      prev.includes(value)
+        ? prev.filter(item => item !== value)
+        : [...prev, value]
+    )
+  }
 
   const { classes } = useStyles()
 
   return (
     <Stack>
       <Breadcrumbs items={breadcrumbs} />
-
       <Title>Avansert personsøk</Title>
       <div className={classes.searchWrapper}>
-        <Stack>
+        <Stack spacing="xl">
           <TextInput
             icon={<IconSearch />}
             placeholder="Søk på navn"
             value={query}
             onChange={evt => setQuery(evt.target.value)}
           />
-
           <Grid>
             <Grid.Col span={1}>Status:</Grid.Col>
-            <Grid.Col span="auto" offset={0.3}>
+            <Grid.Col span="auto" offset={0.2}>
               <Group>
-                <Checkbox label="Aktiv" value="Aktiv" />
+                <Checkbox
+                  label="Aktiv"
+                  checked={status.includes('Aktiv')}
+                  onChange={() => handleCheckboxChange(setStatus, 'Aktiv')}
+                />
                 <Checkbox label="Pang" value="Pang" />
                 <Checkbox label="Nyopptatt" value="Nyopptatt" />
                 <Checkbox label="Nypang" value="Nypang" />
@@ -59,10 +68,9 @@ export const AdvancedSearch = () => {
               </Group>
             </Grid.Col>
           </Grid>
-
           <Grid>
             <Grid.Col span={1}>Gjeng:</Grid.Col>
-            <Grid.Col span="auto" offset={0.3}>
+            <Grid.Col span="auto" offset={0.2}>
               <Group>
                 <Checkbox label="Bar" value="Bar" />
                 <Checkbox label="Sprit" value="Sprit" />
@@ -77,30 +85,28 @@ export const AdvancedSearch = () => {
               </Group>
             </Grid.Col>
           </Grid>
-
           <Grid>
             <Grid.Col span={1}>Verv:</Grid.Col>
-            <Grid.Col span="auto" offset={0.3}>
+            <Grid.Col span="auto" offset={0.2}>
               <Group>
                 <Checkbox label="Gjengis" value="Gjengis" />
                 <Checkbox label="Funksjonær" value="Funksjonær" />
               </Group>
             </Grid.Col>
           </Grid>
-
           <Grid>
-            <Grid.Col span={1}>Socikonto:</Grid.Col>
-            <Grid.Col span="auto" offset={0.3}>
-              <Group>
-                <Radio label="+100kr" value="+100kr" />
-                <Radio label="+200kr" value="+200kr" />
-                <Radio label="+500kr" value="+500kr" />
-                <Radio label="+1000kr" value="+1000kr" />
-              </Group>
+            <Grid.Col span={1}>Konto:</Grid.Col>
+            <Grid.Col span="auto" offset={0.2}>
+              <Radio.Group>
+                <Group>
+                  <Radio label="+100kr" value="+100kr" />
+                  <Radio label="+200kr" value="+200kr" />
+                  <Radio label="+500kr" value="+500kr" />
+                  <Radio label="+1000kr" value="+1000kr" />
+                </Group>
+              </Radio.Group>
             </Grid.Col>
           </Grid>
-
-          <UserGrid search={debouncedQuery} />
         </Stack>
       </div>
     </Stack>
@@ -108,17 +114,8 @@ export const AdvancedSearch = () => {
 }
 
 const useStyles = createStyles(theme => ({
-  button: {
-    width: '10%',
-  },
   searchWrapper: {
     width: '100%',
-  },
-  left: {
-    width: '10%',
-  },
-  right: {
-    width: '10%',
   },
 }))
 
