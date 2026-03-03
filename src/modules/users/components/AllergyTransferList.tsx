47f50a6f38c3cbb7c30388e1b58fbbccab3e7599
Alexander Orvik <alexander@firi.com>
2026-03-03 13:23:14 +0100

    fix(mantine-v7): migrate SimpleGrid breakpoints and replace TransferList

--- src/modules/users/components/AllergyTransferList.tsx
+++ src/modules/users/components/AllergyTransferList.tsx
@@ -46,9 +46,6 @@ export const AllergyTransferList: React.FC<AllergyTransferListProps> = ({
     })
   }
 
-  /**
-   * TODO: https://mantine.dev/combobox/?e=TransferList
-   */
   return (
     <Stack>
       <Title order={3}>Mine allergener</Title>
